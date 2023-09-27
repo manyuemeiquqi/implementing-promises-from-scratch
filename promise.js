3478dc898fb1875dddb4e9df6e9223a7a54e962d
manyuemeiquqi <940495614@qq.com>
2023-09-27 15:08:17 +0800

    feat: update

--- promise.js
+++ promise.js
@@ -37,148 +37,116 @@ class APromise {
 
   then(onFulfilled, onRejected) {
     function resolvePromise(p, value) {
- 
-        if (p === value) {
-            return p.reject(
-              new TypeError("The promise and the return value are the same"),
-            );
-          } else if (typeof value === "object" || typeof value === "function") {
-            if (value === null) {
-              return p.resolve(value);
-            }
-        
-            try {
-              var then = value.then;
-            } catch (error) {
-              return p.reject(error);
-            }
-        
-            if (typeof then === "function") {
-              var called = false;
-              try {
-                then.call(
-                  value,
-                  function (y) {
-                    if (called) return;
-                    called = true;
-                    resolvePromise(p, y);
-                  },
-                  function (r) {
-                    if (called) return;
-                    called = true;
-                    p.reject(r);
-                  },
-                );
-              } catch (error) {
+      if (p === value) {
+        return p.reject(
+          new TypeError("The promise and the return value are the same"),
+        );
+      } else if (typeof value === "object" || typeof value === "function") {
+        if (value === null) {
+          return p.resolve(value);
+        }
+
+        try {
+          var then = value.then;
+        } catch (error) {
+          return p.reject(error);
+        }
+
+        if (typeof then === "function") {
+          var called = false;
+          try {
+            then.call(
+              value,
+              function (y) {
                 if (called) return;
-                p.reject(error);
-              }
-            } else {
-              p.resolve(value);
-            }
-          } else {
-            p.resolve(value);
+                called = true;
+                resolvePromise(p, y);
+              },
+              function (r) {
+                if (called) return;
+                called = true;
+                p.reject(r);
+              },
+            );
+          } catch (error) {
+            if (called) return;
+            p.reject(error);
           }
+        } else {
+          p.resolve(value);
+        }
+      } else {
+        p.resolve(value);
+      }
     }
     var that = this; // 保存一下this
+    let promise2 = new APromise(() => {});
 
     if (this.status === FULFILLED) {
-      var promise2 = new APromise(function (resolve, reject) {
-        setTimeout(function () {
-          try {
-            if (typeof onFulfilled !== "function") {
-              resolve(that.value);
-            } else {
-              var x = onFulfilled(that.value);
-              resolvePromise(promise2, x, resolve, reject);
-            }
-          } catch (error) {
-            reject(error);
+      setTimeout(function () {
+        try {
+          if (typeof onFulfilled !== "function") {
+            promise2.resolve(that.value);
+          } else {
+            var x = onFulfilled(that.value);
+            resolvePromise(promise2, x);
           }
-        }, 0);
-      });
-  
-      return promise2;
+        } catch (error) {
+          promise2.reject(error);
+        }
+      }, 0);
     }
-  
+
     if (this.status === REJECTED) {
-      var promise2 = new APromise(function (resolve, reject) {
-        setTimeout(function () {
-          try {
-            if (typeof onRejected !== "function") {
-              reject(that.value);
-            } else {
-              var x = onRejected(that.value);
-              resolvePromise(promise2, x, resolve, reject);
-            }
-          } catch (error) {
-            reject(error);
+      setTimeout(function () {
+        try {
+          if (typeof onRejected !== "function") {
+            promise2.reject(that.value);
+          } else {
+            var x = onRejected(that.value);
+            resolvePromise(promise2, x);
           }
-        }, 0);
-      });
-  
-      return promise2;
+        } catch (error) {
+          promise2.reject(error);
+        }
+      }, 0);
     }
-  
+
     // 如果还是PENDING状态，将回调保存下来
     if (this.status === PENDING) {
-      var promise2 = new APromise(function (resolve, reject) {
-        that.callbackQueue.push({
-          onFulfilled: function () {
-            setTimeout(function () {
-              try {
-                if (typeof onFulfilled !== "function") {
-                  resolve(that.value);
-                } else {
-                  var x = onFulfilled(that.value);
-                  resolvePromise(promise2, x, resolve, reject);
-                }
-              } catch (error) {
-                reject(error);
+      that.callbackQueue.push({
+        onFulfilled: function () {
+          setTimeout(function () {
+            try {
+              if (typeof onFulfilled !== "function") {
+                promise2.resolve(that.value);
+              } else {
+                var x = onFulfilled(that.value);
+                resolvePromise(promise2, x);
               }
-            }, 0);
-          },
-          onRejected: function () {
-            setTimeout(function () {
-              try {
-                if (typeof onRejected !== "function") {
-                  reject(that.value);
-                } else {
-                  var x = onRejected(that.value);
-                  resolvePromise(promise2, x, resolve, reject);
-                }
-              } catch (error) {
-                reject(error);
+            } catch (error) {
+              promise2.reject(error);
+            }
+          }, 0);
+        },
+        onRejected: function () {
+          setTimeout(function () {
+            try {
+              if (typeof onRejected !== "function") {
+                promise2.reject(that.value);
+              } else {
+                var x = onRejected(that.value);
+                resolvePromise(promise2, x);
               }
-            }, 0);
-          },
-        });
+            } catch (error) {
+              promise2.reject(error);
+            }
+          }, 0);
+        },
       });
-  
-      return promise2;
     }
-    // const returnPromise = new Promise(() => { })
-    // setTimeout(() => {
-    //   if (this.status === PENDING) {
 
-    //   } else {
-    //     if (this.status === FULFILLED) {
-    //       if (typeof onFulfilled !== 'function') {
-    //         returnPromise.resolve(this.value)
-    //       } else {
-    //         try {
-    //           let returnValue = onFulfilled(this.value);
-    //           handlePromise(returnPromise, returnValue)
-    //         } catch (error) {
-    //           returnPromise.reject(error)
-    //         }
-    //       }
-    //     } else {
-
-    //     }
-    //   }
-    // }, 0);
-
+    return promise2;
   }
 }
 module.exports = APromise;
